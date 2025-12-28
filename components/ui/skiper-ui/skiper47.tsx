89727a8cd354aa62e092b9d012cd06ec7d9157d6
BekzodDjumanov <bdjumanov1@gmail.com>
2025-12-27 21:42:00 -0500

    ye

--- components/ui/skiper-ui/skiper47.tsx
+++ components/ui/skiper-ui/skiper47.tsx
@@ -37,7 +37,7 @@ const Skiper47 = () => {
       alt: "Illustrations by my fav AarzooAly",
     },
     {
-      src: "/",
+      src: "/images/bateman.png",
       alt: "Illustrations by my fav AarzooAly",
     },
     {
